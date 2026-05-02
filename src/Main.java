2a5ae3fe3f45914e0f155261621653f86f669313
Serafim550505 <serafimdurekon6317@gmail.com>
2026-05-02 22:56:07 +0300

    домашка 2.0 из 8 заданий

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,73 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.println("задача №!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var  paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
 
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(" Задача №2 ");
+        dog = dog + 4;;
+        cat = cat + 4;;
+        paper = paper + 4;;
+        System.out.println("dog =" + dog);
+        System.out.println("cat =" + cat);
+        System.out.println("paper =" + paper);
+
+
+        System.out.println("Задача №3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+
+
+        System.out.println("Задача №4");
+        var friend = 19;
+        friend = (friend + 2) /7;
+        System.out.println("friend = " + friend);
+
+
+        System.out.println("Задача № 5");
+        var frog = 3.5;
+        frog = ((frog * 10) / 3.5) + 4;
+        System.out.println("frog = " + frog );
+
+
+        System.out.println(" Задача №6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var weɪt = 0;
+        double dɪfrəns = 0;
+        weɪt = (int) (boxer1 + boxer2);
+        System.out.println("общая масса = " + weɪt);
+        dɪfrəns = (int) (boxer2 - boxer1);
+        System.out.println("разница между бойцами = " + dɪfrəns);
+
+
+        System.out.println(" задача №7 ");
+        var qalıq = (int) (weɪt / dɪfrəns);
+        System.out.println("остаток от деления = " + qalıq);
+
+        System.out.println(" задача № 8 ");
+        var taɪm = 8 ;
+        var numberhours = 640;
+        var workers = 0;
+        workers = numberhours / taɪm;
+        System.out.println("всего работников в компании  =  " + workers );
+
+        workers = workers + 94;
+        taɪm = taɪm * workers ;
+        System.out.println("часы работы для всех сотрудников = " + taɪm);
+
+
+
     }
-}
+    }
